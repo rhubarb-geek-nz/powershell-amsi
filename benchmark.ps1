85e3f6749cd564e6479fd9fe6c498585f2208f21
rhubarb-geek-nz <rhubarb-geek-nz@users.sourceforge.net>
2024-05-10 01:02:09 +0100

    Benchmark for Base64 decoding

---
+++ benchmark.ps1
@@ -0,0 +1,102 @@
+#!/usr/bin/env pwsh
+# Copyright (c) 2024 Roger Brown.
+# Licensed under the MIT License.
+
+trap
+{
+	throw $PSItem
+}
+
+$ErrorActionPreference = 'Stop'
+
+try
+{
+	$progressParams = @{
+		Activity = "Benchmark timing"
+		Status = "In progress"
+		PercentComplete = 0
+		CurrentOperation = 'ConvertTo-Base64String'
+	}
+ 
+	Write-Progress @progressParams
+
+	Get-Command -Noun ('Base64String','Reflection')
+
+	$bytes = new-object byte[] -ArgumentList @(,200554320)
+
+	$random = new-object Random
+
+	$random.NextBytes($bytes)
+
+	[string]$base64 = @(,$bytes) | ConvertTo-Base64String
+	$bytes = $null
+
+	$T0 = Get-Date
+
+	$progressParams.PercentComplete = 20
+	$progressParams.CurrentOperation = 'ConvertFrom-Base64String'
+	Write-Progress @progressParams
+
+	$bytes = $base64 | ConvertFrom-Base64String
+	$bytes = $null
+
+	$T1 = Get-Date
+
+	$progressParams.PercentComplete = 40
+	$progressParams.CurrentOperation = '[System.Convert]::FromBase64String'
+	Write-Progress @progressParams
+
+	$bytes = [System.Convert]::FromBase64String($base64)
+	$bytes = $null
+
+	$T2 = Get-Date
+
+	$progressParams.PercentComplete = 60
+	$progressParams.CurrentOperation = 'Invoke-Reflection'
+	Write-Progress @progressParams
+
+	$bytes = Invoke-Reflection -Method FromBase64String -Type ([System.Convert]) -ArgumentDictionary @{ s = $base64 }
+	$bytes = $null
+
+	$T3 = Get-Date
+
+	$progressParams.PercentComplete = 80
+	$progressParams.CurrentOperation = 'Type.GetMethod().Invoke()'
+	Write-Progress @progressParams
+
+	$getMethod=([System.Convert]).GetType().GetMethod('GetMethod',[Type[]](([string],([Type[]]))))
+	$fromBase64String=$getMethod.Invoke(([System.Convert]),[object[]]('FromBase64String', [Type[]](,([string]))))
+	$bytes = $fromBase64String.Invoke($Null,[object[]](,$base64))
+	$bytes = $null
+
+	$T4 = Get-Date
+
+	$progressParams.PercentComplete = 100
+	$progressParams.CurrentOperation = 'Completed'
+	Write-Progress @progressParams
+}
+finally
+{
+	Write-Progress -Completed -Activity "Completed"
+}
+
+@(
+	[pscustomobject]@{
+		TestCase = 'ConvertFrom-Base64String'
+		Duration = [int]($T1-$T0).TotalMilliseconds
+	},
+	[pscustomobject]@{
+		TestCase = '[System.Convert]::FromBase64String()'
+		Duration = [int]($T2-$T1).TotalMilliseconds
+	},
+	[pscustomobject]@{
+		TestCase = 'Invoke-Reflection'
+		Duration = [int]($T3-$T2).TotalMilliseconds
+	},
+	[pscustomobject]@{
+		TestCase = 'Type.GetMethod().Invoke()'
+		Duration = [int]($T4-$T3).TotalMilliseconds
+	}
+) | Format-Table
+
+$PSVersionTable
